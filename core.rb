76fbd6756a2900503b2474d5c862b5d2943e3c0b
ctopjian <charlietopjian@gmail.com>
2015-06-28 17:01:49 -0500

    uupdated .gitignore file

--- core.rb
+++ core.rb
@@ -8,43 +8,15 @@ require 'rmagick'
 include Magick
 
 
-def makeImg (txt, outImg)
-
-	imgWidth	=	200
-	imgHeight	=	400
-
-	canvas = Magick::ImageList.new
-	#canvas.new_image(imgWidth,imgHeight)
-	canvas = Magick::ImageList.new("inherent.jpg")
-
-	text = Magick::Draw.new
-	
-	text.annotate(canvas, 0,0,0,0, txt) {
-		self.fill = "black"
-
-		self.font_family = "arial"
-		self.pointsize	=	25
-		self.gravity = CenterGravity
-	}	
-
-	#create rectangle to have some bg
-	text.rectangle(75, 35,0,0) {
-		self.fill = 'white'	
-		self.stroke = 'white'
-	}
-	text.draw(canvas)
-	
-
-	canvas.write(outImg)
-	
-end
-
+#simple method to take passed string and overlay
+#on top of an image.
 def makeImg2 (txt, outImg)
 
 
 	img = Magick::Image.read('inherent.jpg').first
-	drw = Magick::Draw.new
 
+	#create text square (layer)
+	drw = Magick::Draw.new
 	drw.fill = 'white'
 	drw.fill_opacity(0.65)
 	drw.stroke = 'white'
@@ -52,12 +24,25 @@ def makeImg2 (txt, outImg)
 	drw.stroke_width(3)
 	drw.stroke_linecap('round')
 	drw.stroke_linejoin('round')
-	drw.rectangle(75,250,300,325)
-	#drw.gravity = CenterGravity
+	drw.rectangle(75,265,325,340)
 	drw.draw(img)
+	
+	#create text annotate
+	text = Magick::Draw.new	
+	text.annotate(img, 0,0,0,0, txt) {
+		self.fill = "black"
+
+		self.font_family = "arial"
+		self.pointsize	=	18
+		self.gravity = CenterGravity
+	}	
+
+	#text.draw(img)
+
 	img.write outImg
 
 end
+
 
 CSV.foreach('code.csv') do |row|
 	puts row[0] + '.gif'
